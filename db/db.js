0fff3c7cf81ec86a1dac7fb961c52398619454bb
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2020-11-11 19:23:34 +0100

    Add basic sqlite queries

--- db/db.js
+++ db/db.js
@@ -2,36 +2,50 @@ import * as SQLite from 'expo-sqlite';
 
 const db = SQLite.openDatabase('days.db');
 
-export const init = () => {
-  const promise = new Promise((resolve, reject) => {
+const createOperation = (query, data = []) => {
+  return new Promise((resolve, reject) => {
     db.transaction(tx => {
-      tx.executeSql('CREATE TABLE IF NOT EXISTS days (id INTEGER PRIMARY KEY NOT NULL, date STRING NOT NULL UNIQUE, startHour TEXT NOT NULL, endHour TEXT NOT NULL);', 
-      [],
-      () => {
-        resolve();
-      },
-      (_, err) => {
-        reject(err);
-      } 
-      );
-    });
+      tx.executeSql(
+        query, 
+        data,
+        (_, result) => {
+          resolve(result)
+        },
+        (_, err) => {
+          reject(err);
+        }
+      )
+    })
   });
-  return promise;
 }
 
+export const init = () => createOperation(
+  'CREATE TABLE IF NOT EXISTS days (id INTEGER PRIMARY KEY NOT NULL, date STRING NOT NULL UNIQUE, startHour TEXT NOT NULL, endHour TEXT NOT NULL);'
+);
+
 export const insertDay = ({ dateString, startHour, endHour }) => {
-  const promise = new Promise((resolve, reject) => {
-    db.transaction(tx => {
-      tx.executeSql('INSERT INTO days (date, startHour, endHour) VALUES (?, ? , ?)',  
-      [dateString, startHour, endHour],
-      (_, result) => {
-        resolve(result);
-      },
-      (_, err) => {
-        reject(err);
-      } 
-      );
-    });
-  });
-  return promise;
+  return createOperation(
+    'INSERT INTO days (date, startHour, endHour) VALUES (?, ? , ?);', 
+    [dateString, startHour, endHour],
+  );
+}
+
+export const updateDay = ({ id, startHour, endHour }) => {
+  return createOperation(
+    'UPDATE days SET startHour = ?, endHour = ? WHERE id = ?', 
+    [startHour, endHour, id],
+  );
+}
+
+export const getAllResults = () => {
+  return createOperation(
+    'SELECT * FROM days;',  
+  );
+}
+
+export const getSpecificDays = ({ startDate, endDate }) => {
+  return createOperation(
+    'SELECT * FROM days WHERE date BETWEEN ? and ?',
+    [startDate, endDate],
+  );
 }
